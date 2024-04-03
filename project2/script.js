677e89dea80dc736947dffe3a9f0746045d533d0
vsvishwas03 <vsvishwas03@gmail.com>
2024-04-03 12:44:16 +0530

    git commit -m "complete project2"

--- project2/script.js
+++ project2/script.js
@@ -1,7 +1,10 @@
 window.addEventListener("mousemove", function(details){
+
+    var rect=document.querySelector("#rect");
+   var xval= gsap.utils.mapRange(0,window.innerWidth,100+rect.getBoundingClientRect().width/2,window.innerWidth-100,details.clientX);
     gsap.to('#rect',{
-        left: details.clientX +"px",
-        ease:Power3
+        left: xval +"px",
+        ease:Power3,
 
 
     });
